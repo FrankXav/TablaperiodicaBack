21f413459cf7b590fdf312cbc4c229cacca77df7
Francisco Xavier Ramirez Leon <framirezl1701@alumno.ipn.mx>
2022-01-24 14:50:01 -0600

    arreglar urls

--- Tablaperiodica/urls.py
+++ Tablaperiodica/urls.py
@@ -4,7 +4,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from .views import *
 
 #from django.conf import settings
 #from django.conf.urls.static import static
@@ -23,7 +22,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('', index),
     path('admin/', admin.site.urls),
     path('', include('Elementos.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
